62d5d82ee76e7f6238fb132e49a50ca2592bcf05
Ubaid2116 <ukubaid74@gmail.com>
2025-07-08 00:57:58 +0500

    update

--- api/main.py
+++ api/main.py
@@ -56,7 +56,7 @@ run_config = RunConfig(
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://multi-agents-openai.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
